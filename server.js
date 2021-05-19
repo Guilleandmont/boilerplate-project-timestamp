917fe8c824b421f7a521eaed10d8f32483a238d7
Guillermo <guilleandmont@gmail.com>
2021-05-18 20:37:50 -0400

    got api working for all cases

--- server.js
+++ server.js
@@ -19,11 +19,24 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/:date?", function (req, res) {
+  let timeString = req.params.date ?? new Date();
+  if (/^\d+$/.test(timeString)) {
+    timeString = parseInt(timeString);
+  }
+  const time = new Date(timeString);
+
+  res.json({
+    unix: time.getTime(),
+    utc: time.toUTCString(),
+    console: time.valueOf(),
+  });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(1000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+const time = new Date();
+console.log(time);
